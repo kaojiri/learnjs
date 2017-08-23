c5eef16486de1d9aa7c8a37fc6724ffa5c424906
koji aizawa <koji.aizawa@unisys.co.jp>
2017-08-24 00:07:11 +0900

    Finished chapter4.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,10 @@
 'use strict';
-var learnjs = {};
+
+var learnjs = {
+    poolId: 'ap-northeast-1:aefcbf63-579f-4a5f-abed-2cb7904c05e2'
+};
+
+learnjs.identity = new $.Deferred();
 
 learnjs.problems = [
     {
@@ -12,26 +17,26 @@ learnjs.problems = [
     }
 ];
 
-learnjs.applyObject = function(obj, elem){
-    for ( var key in obj) {
+learnjs.applyObject = function (obj, elem) {
+    for (var key in obj) {
         elem.find('[data-name="' + key + '"]').text(obj[key]);
     }
 };
 
-learnjs.flashElement = function(elem, content){
-    elem.fadeOut('fast', function(){
+learnjs.flashElement = function (elem, content) {
+    elem.fadeOut('fast', function () {
         elem.html(content);
         elem.fadeIn();
-    });  
+    });
 }
 
-learnjs.template = function(name) {
+learnjs.template = function (name) {
     return $('.templates .' + name).clone();
 }
 
-learnjs.triggerEvent = function(name,args){
+learnjs.triggerEvent = function (name, args) {
     // view-container要素配下のすべての要素に対して、nameで渡されてきた名前のイベントをトリガーする
-    $('.view-container>*').trigger(name,args);
+    $('.view-container>*').trigger(name, args);
 }
 
 learnjs.buildCorrectFlash = function (problemNum) {
@@ -39,24 +44,30 @@ learnjs.buildCorrectFlash = function (problemNum) {
     var link = correctFlash.find('a');
     if (problemNum < learnjs.problems.length) {
         link.attr('href', '#problem-' + (problemNum + 1));
-    } else{
+    } else {
         link.attr('href', '');
         link.text("You're Finished!");
     }
     return correctFlash;
 }
 
-learnjs.landingView = function() {
+learnjs.addProfileLink = function (profile) {
+    var link = learnjs.template('profile-link');
+    link.find('a').text(profile.email);
+    $('.signin-bar').prepend(link);
+}
+
+learnjs.landingView = function () {
     return learnjs.template('landing-view');
 }
 
-learnjs.problemView = function(data) {
-    var problemNumber = parseInt(data,10);
+learnjs.problemView = function (data) {
+    var problemNumber = parseInt(data, 10);
     var view = learnjs.template('problem-view');
-    var problemData = learnjs.problems[problemNumber -1];
+    var problemData = learnjs.problems[problemNumber - 1];
     var resultFlash = view.find('.result');
 
-    function checkAnswer(){
+    function checkAnswer() {
         var answer = view.find('.answer').val();
         var test = problemData.code.replace('__', answer) + '; problem();';
 
@@ -64,11 +75,11 @@ learnjs.problemView = function(data) {
         return eval(test);
     }
 
-    function checkAnswerClick(){
-        if (checkAnswer()){
+    function checkAnswerClick() {
+        if (checkAnswer()) {
             var correctFlash = learnjs.buildCorrectFlash(problemNumber);
             learnjs.flashElement(resultFlash, correctFlash);
-        } else{
+        } else {
             learnjs.flashElement(resultFlash, 'Incorrect!');
         }
         return false;
@@ -77,13 +88,13 @@ learnjs.problemView = function(data) {
     if (problemNumber < learnjs.problems.length) {
         // skipボタンの定義
         var buttonItem = learnjs.template('skip-btn');
-        buttonItem.find('a').attr('href','#problem-' + (problemNumber + 1));
+        buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
 
         // nav-listクラスの要素にbuttonItemを追加
         $('.nav-list').append(buttonItem);
 
         // removingViewという名前のカスタムイベントをバインドしている。その処理の中身はbuttonItem.removeという意味
-        view.bind('removingView', function(){
+        view.bind('removingView', function () {
             buttonItem.remove();
         });
     }
@@ -95,30 +106,78 @@ learnjs.problemView = function(data) {
     return view;
 }
 
+learnjs.profileView = function () {
+    var view = learnjs.template('profile-view');
+    learnjs.identity.done(function (identity) {
+        view.find('.email').text(identity.email);
+    });
+    return view;
+}
+
 // 引数はview名で、この中のroutesのシンボルを指す
-learnjs.showView = function(hash) {
+learnjs.showView = function (hash) {
     var routes = {
         '#problem': learnjs.problemView,
+        '#profile': learnjs.profileView,
         '#': learnjs.landingView,
         '': learnjs.landingView
     };
     var hashParts = hash.split('-');
     var viewFn = routes[hashParts[0]];
-    if (viewFn){
+    if (viewFn) {
         // view遷移の前に、removingViewのイベントをトリガーする
-        learnjs.triggerEvent('removingView',[]);
-        $('.view-container').empty().append(viewFn(hashParts[1]));        
+        learnjs.triggerEvent('removingView', []);
+        $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
 
-learnjs.appOnReady = function(){
-    window.onhashchange = function(){
+learnjs.appOnReady = function () {
+    window.onhashchange = function () {
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
+    learnjs.identity.done(learnjs.addProfileLink);
 }
 
-function googleSignIn(){
-    console.log(arguments);
-    console.log("This is a test.");
+learnjs.awsRefresh = function () {
+    var deferred = new $.Deferred();
+    AWS.config.credentials.refresh(function (err) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(AWS.config.credentials.identityId);
+        }
+    });
+    return deferred.promise();
 }
+
+function googleSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    AWS.config.update({
+        region: 'ap-northeast-1',
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: learnjs.poolId,
+            Logins: {
+                'accounts.google.com': id_token
+            }
+        })
+    })
+    function refresh() {
+        return gapi.auth2.getAuthInstance().signIn({
+            prompt: 'login'
+        }).then(function (userUpdate) {
+            var creds = AWS.config.credentials;
+            var newToken = userUpdate.getAuthResponse().id_token;
+            creds.params.Logins['accounts.google.com'] = newToken;
+            return learnjs.awsRefresh();
+        });
+    }
+    learnjs.awsRefresh().then(function (id) {
+        learnjs.identity.resolve({
+            id: id,
+            email: googleUser.getBasicProfile().getEmail(),
+            refresh: refresh
+        });
+    });
+}
+
